e4de3f66f64869caf16f6e0f6a1d5066c69a1ac6
Noel1217 <62556968+Noel1217@users.noreply.github.com>
2020-11-18 19:52:08 -0500

    init react and added express to backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,5 @@
-console.log("Test")
+const express = require("express");
+const app = express(); // creating app using express
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT,(err) => !err ? console.log("Started Server") : console.log("Error Starting server")) //Starting server on enviroment port or 5000
+app.use(express.static("public"));
