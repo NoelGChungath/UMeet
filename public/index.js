d724095b4d1acb796c5aa948b2953c8a7c46f47a
Noel1217 <62556968+Noel1217@users.noreply.github.com>
2020-11-19 22:06:37 -0500

    added in ice server from google

--- public/index.js
+++ public/index.js
@@ -1,9 +1,14 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+const myPeer = new Peer({
+    config: {"iceServers": [
+      
+      {
+        urls:"stun:stun.l.google.com:19302"
+      }
+    ],
+  }
+});
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
